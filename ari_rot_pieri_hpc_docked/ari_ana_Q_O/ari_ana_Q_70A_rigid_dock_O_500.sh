83dd71298f42d9d187b79e4b7ff06347caffd101
Yizhou Yu <32229268+izu0421@users.noreply.github.com>
2021-07-04 23:41:14 +0100

    Delete ari_rot_pieri_hpc_docked directory

--- ari_rot_pieri_hpc_docked/ari_ana_Q_O/ari_ana_Q_70A_rigid_dock_O_500.sh
+++
@@ -1,6 +0,0 @@
-for receptor in mitoC1*.pdbqt
-do
-for ligand in lig_*.pdbqt
-do ./vina --ligand $ligand --receptor $receptor --log log_${ligand%.*}_${receptor%.*}_500.txt --config *config.txt --out out__${ligand%.*}_${receptor%.*}_500.pdbqt
-done
-done
--- ari_rot_pieri_hpc_docked/hpc_loop_command.sh
+++
@@ -1,6 +0,0 @@
-for folder in *Q_O
-do 
-cd $folder
-./*.sh
-cd ../
-done
